5dd1adc7d73a12042b23ce6c2e3ce854bd7b3100
rajan <royal_raj@outlook.com>
2020-05-27 13:31:41 +0545

    Adds razor to separate file using base class

---
+++ BlazorServerApp/Pages/FetchDataBase.cs
@@ -0,0 +1,25 @@
+﻿using BlazorServerApp.Data;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServerApp.Pages
+{
+    public class FetchDataBase : ComponentBase
+    {
+        public WeatherForecastService ForecastService { get; }
+        protected WeatherForecast[] forecasts;
+
+        public FetchDataBase()
+        {
+            ForecastService = new WeatherForecastService();
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            forecasts = await ForecastService.GetForecastAsync(DateTime.Now);
+        }
+    }
+}
