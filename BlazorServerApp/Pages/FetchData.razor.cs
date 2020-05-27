c3342dbe126502c42fdbaffa386e960263ef07e4
rajan <royal_raj@outlook.com>
2020-05-27 14:28:28 +0545

    when separating razor to separate file, use partial class if you need to inject services, use base class approach if you dont need to inject service

---
+++ BlazorServerApp/Pages/FetchData.razor.cs
@@ -0,0 +1,24 @@
+﻿using BlazorServerApp.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServerApp.Pages
+{
+    public partial class FetchData
+    {
+        //public WeatherForecastService ForecastService { get; }
+        protected WeatherForecast[] forecasts;
+
+        public FetchData()
+        {
+            //ForecastService = weatherForecastService;
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            forecasts = await ForecastService.GetForecastAsync(DateTime.Now);
+        }
+    }
+}
