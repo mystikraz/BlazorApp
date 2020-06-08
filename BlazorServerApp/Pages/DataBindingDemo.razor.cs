cb28a7fe8ff81f30dafe2324e6965c973dbd05b8
rajan <royal_raj@outlook.com>
2020-06-08 12:08:25 +0545

    Two way binding

---
+++ BlazorServerApp/Pages/DataBindingDemo.razor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServerApp.Pages
+{
+    public partial class DataBindingDemo
+    {
+        public string Name { get; set; } = "Tom";
+        public string Gender { get; set; } = "Male";
+        public string Colour { get; set; } = "background-color:white";
+    }
+}
