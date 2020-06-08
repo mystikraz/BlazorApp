83041895d06c649198013abb44d066c016fb2406
rajan <royal_raj@outlook.com>
2020-06-08 12:16:46 +0545

    Two way binding in detail

--- BlazorServerApp/Pages/DataBindingDemo.razor.cs
+++ BlazorServerApp/Pages/DataBindingDemo.razor.cs
@@ -10,5 +10,6 @@ namespace BlazorServerApp.Pages
         public string Name { get; set; } = "Tom";
         public string Gender { get; set; } = "Male";
         public string Colour { get; set; } = "background-color:white";
+        public string Description { get; set; }
     }
 }
