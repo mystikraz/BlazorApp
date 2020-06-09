168a487bf61160eee8d7fe639173427f8c950a96
rajan <royal_raj@outlook.com>
2020-06-09 12:26:32 +0545

    Parent child components

---
+++ BlazorServerApp/Pages/DisplayEmployee.razor.cs
@@ -0,0 +1,17 @@
+﻿using BlazorApp.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServerApp.Pages
+{
+    public partial class DisplayEmployee
+    {
+        [Parameter]
+        public Employee Employee { get; set; }
+        [Parameter]
+        public bool ShowFooter { get; set; }
+    }
+}
--- BlazorServerApp/Pages/EmployeeListBase.cs
+++ BlazorServerApp/Pages/EmployeeListBase.cs
@@ -14,6 +14,7 @@ namespace BlazorServerApp.Pages
         [Inject]
         public IEmployeeService EmployeeService { get; set; }
         public IEnumerable<Employee> Employees { get; set; }
+        public bool ShowFooter { get; set; } = true;
         protected override async Task OnInitializedAsync()
         {
             Employees=(await EmployeeService.GetEmployees()).ToList();
